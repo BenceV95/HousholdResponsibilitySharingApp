b4c873f0512b3cda6b8368f48c9be25fe8862bea
Bálint <szelasszie@gmail.com>
2025-01-20 16:03:19 +0100

    Basic scaffolding for project is done

---
+++ HouseholdResponsibilityAppServerTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace HouseholdResponsibilityAppServerTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
