3f283d0e4eb60eadc65923c9f84d46138e6bc440
Bálint <szelasszie@gmail.com>
2025-02-03 13:30:30 +0100

    Add: DbContext class -task class

---
+++ HouseholdResponsibilityAppServer/Context/HouseholdResponsibilityAppContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HouseholdResponsibilityAppServer.Context
+{
+    public class HouseholdResponsibilityAppContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<ScheduledTask> ScheduledTasks { get; set; }
+        public DbSet<History> Histories { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Household> Households { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+    }
+}
---
+++ HouseholdResponsibilityAppServer/Models/Task/Task.cs
@@ -0,0 +1,15 @@
+﻿namespace HouseholdResponsibilityAppServer.Models.Task
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int GroupId { get; set; }
+        // if its a bool, change name later?
+        public bool Priority { get; set; }
+
+    }
+}
