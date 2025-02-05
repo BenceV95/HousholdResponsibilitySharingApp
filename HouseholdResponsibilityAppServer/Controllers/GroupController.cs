8332e098915d819f432fa272f7f5d7feb6e4cbb3
Bukovinszki Ákos <nashyro1995@gmail.com>
2025-02-05 09:21:37 +0100

    feat: Add initial controllers for Group, Household, and User management

---
+++ HouseholdResponsibilityAppServer/Controllers/GroupController.cs
@@ -0,0 +1,94 @@
+﻿using HouseholdResponsibilityAppServer.DTOs;
+using HouseholdResponsibilityAppServer.Services;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+public class GroupController : ControllerBase
+{
+    private readonly IGroupService _groupService;
+
+    public GroupController(IGroupService groupService)
+    {
+        _groupService = groupService;
+    }
+
+    [HttpGet("/groups")]
+    public async Task<ActionResult> GetAllGroup()
+    {
+        try
+        {
+            var groups = await _groupService.GetAllGroupsAsync();
+            return Ok(groups);
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine(ex.Message);
+
+            return BadRequest("An error occurred while retrieving groups.");
+        }
+    }
+
+    [HttpGet("/group/{groupId}")]
+    public async Task<ActionResult> GetGroupById(int groupId)
+    {
+        try
+        {
+            var group = await _groupService.GetGroupByIdAsync(groupId);
+            return Ok(group);
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine(ex.Message);
+
+            return BadRequest("An error occurred while retrieving groups.");
+        }
+    }
+
+    [HttpPost("/group")]
+    public async Task<ActionResult> CreateGroup([FromBody] GroupDto groupDto)
+    {
+        try
+        {
+            await _groupService.CreateGroupAsync(groupDto);
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine(ex.Message);
+
+            return BadRequest("An error occurred while creating group.");
+        }
+    }
+
+    [HttpPut("/group/{groupId}")]
+    public async Task<ActionResult> UpdateGroup(int groupId, [FromBody] GroupDto groupDto)
+    {
+        try
+        {
+            await _groupService.UpdateGroupAsync(groupId, groupDto);
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine(ex.Message);
+
+            return BadRequest("An error occurred while updating group.");
+        }
+    }
+
+    [HttpDelete("/group/{groupId}")]
+    public async Task<ActionResult> DeleteGroup(int groupId)
+    {
+        try
+        {
+            await _groupService.DeleteGroupAsync(groupId);
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine(ex.Message);
+
+            return NotFound("An error occurred while deleting group.");
+        }
+    }
+}
