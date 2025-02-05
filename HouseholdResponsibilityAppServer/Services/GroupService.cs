0d704f61e18e73b3ca20be370e8c47fc917b719d
Bukovinszki Ákos <nashyro1995@gmail.com>
2025-02-05 09:23:45 +0100

    feat: Add service layer for business logic

---
+++ HouseholdResponsibilityAppServer/Services/GroupService.cs
@@ -0,0 +1,66 @@
+﻿using HouseholdResponsibilityAppServer.DTOs;
+using HouseholdResponsibilityAppServer.Models;
+using HouseholdResponsibilityAppServer.Repositories;
+using Microsoft.AspNetCore.Identity;
+using System.Text.RegularExpressions;
+
+namespace HouseholdResponsibilityAppServer.Services
+{
+    public class GroupService : IGroupService
+    {
+        private readonly IGroupRepository _groupRepository;
+
+        public GroupService(IGroupRepository groupRepository)
+        {
+            _groupRepository = groupRepository;
+        }
+
+        public async Task<IEnumerable<GroupResponseDto>> GetAllGroupsAsync()
+        {
+            var groups = await _groupRepository.GetAllGroupsAsync();
+
+            return groups.Select(group => new GroupResponseDto
+            {
+                GroupResponseDtoId = group.GroupId,
+                Name = group.Name,
+            }).ToList();
+
+        }
+
+        public async Task<GroupResponseDto> GetGroupByIdAsync(int groupId)
+        {
+            var group = await _groupRepository.GetGroupByIdAsync(groupId);
+
+            return new GroupResponseDto
+            {
+                GroupResponseDtoId = group.GroupId,
+                Name = group.Name,
+            };
+        }
+
+        public async Task CreateGroupAsync(GroupDto groupDto)
+        {
+            var group = new TaskGroup
+            {
+                Name = groupDto.Name,
+            };
+
+            await _groupRepository.AddGroupAsync(group);
+        }
+
+        public async Task UpdateGroupAsync(int groupId, GroupDto groupDto)
+        {
+            var group = await _groupRepository.GetGroupByIdAsync(groupId);
+
+            group.Name = groupDto.Name;
+
+            await _groupRepository.UpdateGroupAsync(group);
+        }
+
+        public async Task DeleteGroupAsync(int groupId)
+        {
+            await _groupRepository.DeleteGroupAsync(groupId);
+        }
+
+    }
+}
